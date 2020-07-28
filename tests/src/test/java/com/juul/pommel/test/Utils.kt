ea7bcfba39c50191f713a8c0137acb37da52a662
kevin.velasco <kevin.velasco@gmail.com>
2020-07-28 16:40:29 -0700

    fix comment

--- tests/src/test/java/com/juul/pommel/test/Utils.kt
+++ tests/src/test/java/com/juul/pommel/test/Utils.kt
@@ -2,7 +2,6 @@ package com.juul.pommel.test
 
 import com.tschuchort.compiletesting.KotlinCompilation
 import org.assertj.core.api.AbstractFileAssert
-import org.assertj.core.api.AbstractStringAssert
 import org.intellij.lang.annotations.Language
 import java.io.File
 
@@ -11,7 +10,7 @@ fun KotlinCompilation.Result.getGeneratedFile(name: String): File =
         "Unable to find generated file: $name"
     }
 
-/** Wrapper around [AbstractStringAssert.isEqualTo] for Java code literals to syntax highlight in Android Studio. */
+/** Wrapper around [AbstractFileAssert.hasContent] for Java code literals to syntax highlight in Android Studio. */
 fun AbstractFileAssert<*>.isEqualToJava(
     @Language("java") source: String
 ): AbstractFileAssert<*> = this.hasContent(source.trimIndent())
